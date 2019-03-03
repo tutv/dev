10eb0365a39b5a593dd8c99f5c23371dd3efc44e
Max <tutv95@gmail.com>
2019-03-03 22:20:32 +0700

    Added robots.txt

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+const fs = require('fs')
+const {join} = require('path')
+const {promisify} = require('util')
+const copyFile = promisify(fs.copyFile)
+
 module.exports = {
-    exportPathMap: async function (defaultPathMap) {
+    exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
+        if (dev) {
+            return defaultPathMap
+        }
+
+        await copyFile(join(dir, 'static', 'robots.txt'), join(outDir, 'robots.txt'))
+
         return {
             '/': {page: '/'},
             '/cv': {page: '/cv'},
