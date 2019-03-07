764f6501f5281e94cd84a06a3c6c16bc567b4b1e
Max <tutv95@gmail.com>
2019-03-08 00:00:52 +0700

    Updated summary

---
+++ src/app/resume/components/EmploymentHistory.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class EmploymentHistory extends Component {
+    render() {
+        return (
+            <div className="EmploymentHistory">
+                <h3 className="SectionTitle">Employment history</h3>
+
+                <div className="MainSection">
+                    <ul className="Listing">
+                        <li>3+ years of experience in the manufacturing industry</li>
+                        <li>Solid background in process development, product quality control and equipment design</li>
+                        <li>Boosted company sales by 30% within a 2-year period by establishing new products and
+                            clients
+                        </li>
+                        <li>Proficient in MS Office, Flash, HTML and Adobe Photoshop</li>
+                        <li>Multilingual: Fluent in English and Vietnamese</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default EmploymentHistory
--- src/app/resume/components/ResumePage.js
+++ src/app/resume/components/ResumePage.js
@@ -2,14 +2,20 @@ import React, {Component} from 'react'
 import ResumeHeader from "./ResumeHeader"
 import Qualifications from "./Qualifications"
 import SkillAndExperience from "./SkillAndExperience"
+import EmploymentHistory from "./EmploymentHistory"
+import EducationHistory from "./EducationHistory"
+import Summary from "./Summary"
 
 class ResumePage extends Component {
     render() {
         return (
             <div className="ResumePage">
                 <ResumeHeader/>
+                <Summary/>
                 <Qualifications/>
                 <SkillAndExperience/>
+                <EmploymentHistory/>
+                <EducationHistory/>
             </div>
         )
     }
