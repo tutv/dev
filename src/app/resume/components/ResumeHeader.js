459acafde7f2b386cd2734a2f050510b8db0db03
Max <tutv95@gmail.com>
2019-04-13 08:25:02 +0700

    Updated address

--- src/app/resume/components/ResumeHeader.js
+++ src/app/resume/components/ResumeHeader.js
@@ -7,7 +7,7 @@ class ResumeHeader extends Component {
                 <h1 className="Name">Tran Van Tu</h1>
 
                 <div className="SubHead">
-                    <span className="Address">24 Nguyen Co Thach, My Dinh, Nam Tu Liem, Ha Noi</span>
+                    <span className="Address">27 Do Xuan Hop, My Dinh 1, Nam Tu Liem, Ha Noi</span>
                     <a className="Phone" href="tel:+84373428920">+84373428920</a>
                     <a className="Email" href="mailto:tutv95@gmail.com">tutv95@gmail.com</a>
                 </div>
