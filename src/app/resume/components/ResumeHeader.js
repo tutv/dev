30faca6eaa7614c497a62ddbffe2cfcd590ced16
tutv <tutv@foobla.com>
2022-02-07 14:09:43 +0700

    update address

--- src/app/resume/components/ResumeHeader.js
+++ src/app/resume/components/ResumeHeader.js
@@ -7,7 +7,7 @@ class ResumeHeader extends Component {
                 <h1 className="Name">Tran Van Tu</h1>
 
                 <div className="SubHead">
-                    <span className="Address">27 Do Xuan Hop, My Dinh 1, Nam Tu Liem, Ha Noi</span>
+                    <span className="Address">S202, Vinhomes Smart City, Tay Mo, Nam Tu Liem, Ha Noi</span>
                     <a className="Phone" href="tel:+84373428920">+84373428920</a>
                     <a className="Email" href="mailto:tutv95@gmail.com">tutv95@gmail.com</a>
                 </div>
